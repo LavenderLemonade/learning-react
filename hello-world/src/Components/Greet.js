bb35e9015de5a0eeb478e560c63113e96e99df11
Sam Elliott <soe1@uakron.edu>
2022-11-09 15:21:19 -0500

    making sure my components are updated

---
+++ hello-world/src/Components/Greet.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Greet = () => <h1> Hey Sam!</h1> //this is the ES6 way of doing arrow functions
+
+export default Greet //we use default so that we can change the name and stuff when we import it
