3abc19bb1277c8152e14553be0042ab74b580a6c
Sam Elliott <soe1@uakron.edu>
2022-11-09 16:37:21 -0500

    learning how the state works! Added in the Message component!

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -2,13 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './Components/Greet.js';
 import Welcome from './Components/Welcome';
+import Message from './Components/Message';
 
 function App() {
   return (
     <div className="App">
-      <Greet />
+      {/* <Greet name = "Bruce" heroName = "Batman">
+        <p> this is children props </p> </Greet>
+      <Greet name = "Clark" heroName = "Superman"> 
+      <button> Hey</button></Greet>
+      <Greet name = "Diana" heroName = "Wonder Woman"/>
 
-      <Welcome> </Welcome>
+      <Welcome name = "Bruce" heroName = "Batman">  </Welcome>
+      <Welcome name = "Clark" heroName = "Superman">  </Welcome>
+      <Welcome name = "Diana" heroName = "Wonder Woman">  </Welcome> */}
+      <Message> </Message>
+
     </div>
   );
 }
--- hello-world/src/Components/Greet.js
+++ hello-world/src/Components/Greet.js
@@ -1,5 +1,18 @@
 import React from "react";
 
-const Greet = () => <h1> Hey Sam!</h1> //this is the ES6 way of doing arrow functions
+const Greet = (props) => {
+console.log(props)
+return (
+    <div>
+        <h1> Hello {props.name}! You are {props.heroName}!</h1> 
+        {props.children} 
+    </div>
+    )
+}
 
 export default Greet //we use default so that we can change the name and stuff when we import it
+
+
+/*this is the ES6 way of doing arrow functions */
+
+//we're throwing it all into a div because we can only return one HTML element inside a Component
