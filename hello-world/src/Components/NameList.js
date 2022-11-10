b6337675c2860a0e161ab026010c8b5ccd76e342
Sam Elliott <soe1@uakron.edu>
2022-11-10 10:49:10 -0500

    learning more about the map function

--- hello-world/src/Components/NameList.js
+++ hello-world/src/Components/NameList.js
@@ -2,16 +2,18 @@ import React from 'react'
 
 function NameList() {
     const names = ['Bruce', 'Clark', 'Diana']
-    const nameList = names.map((name) => <h2> {name} </h2>)
+    const nameList = names.map(name => <h2 key = {name}> {name} </h2>)
   return (
     <div>
         {/* {
             names.map((name) => <h2> {name} </h2>) so we could do it here or up there 
         } */}
 
-        {nameList}
+        {nameList} 
     </div>
   )
 }
 
 export default NameList
+
+//refactoring the JSX to be outside of the render method is a good thing to do
