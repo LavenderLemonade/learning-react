71367d3e86c5e2164ca7d5523e48c36a3106c229
Sam Elliott <soe1@uakron.edu>
2022-11-09 19:38:01 -0500

    learning about state-based stuff right now!

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -3,10 +3,15 @@ import './App.css';
 import Greet from './Components/Greet.js';
 import Welcome from './Components/Welcome';
 import Message from './Components/Message';
+import Counter from './Components/Counter';
 
 function App() {
   return (
     <div className="App">
+
+      {/* <Greet /> */}
+
+    <Counter />
       {/* <Greet name = "Bruce" heroName = "Batman">
         <p> this is children props </p> </Greet>
       <Greet name = "Clark" heroName = "Superman"> 
@@ -16,7 +21,7 @@ function App() {
       <Welcome name = "Bruce" heroName = "Batman">  </Welcome>
       <Welcome name = "Clark" heroName = "Superman">  </Welcome>
       <Welcome name = "Diana" heroName = "Wonder Woman">  </Welcome> */}
-      <Message> </Message>
+      {/* <Message> </Message> */}
 
     </div>
   );
